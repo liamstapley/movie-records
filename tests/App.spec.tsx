370460b42e9f1236f115d4ec411021fa2579f654
liamstapley <stapley.liam@gmail.com>
2026-04-28 14:43:49 -0400

    added act to the test's

--- tests/App.spec.tsx
+++ tests/App.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, act } from "@testing-library/react";
 import App from "../src/App";
 
 describe("App Component", () => {
@@ -12,52 +12,70 @@ describe("App Component", () => {
         render(<App />);
         const addButton = screen.getAllByText(/Mark as watched/i);
         expect(addButton.length > 0);
-        addButton[0].click();
+        act(() => {
+            addButton[0].click();
+        });
     });
 
     test("editMovie function exists", () => {
         render(<App />);
         const addButton = screen.getAllByText(/Edit/i);
         expect(addButton.length > 0);
-        addButton[0].click();
+        act(() => {
+            addButton[0].click();
+        });
         render(<App />);
         const saveButton = screen.getByRole("button", { name: /Save/i });
         expect(saveButton);
-        saveButton.click();
+        act(() => {
+            saveButton.click();
+        });
     });
 
     test("deleteMovie function exists", () => {
         render(<App />);
         const addButton = screen.getAllByText(/Edit/i);
         expect(addButton.length > 0);
-        addButton[0].click();
+        act(() => {
+            addButton[0].click();
+        });
         render(<App />);
         const deleteButton = screen.getByRole("button", { name: /Delete/i });
         expect(deleteButton);
-        deleteButton.click();
+        act(() => {
+            deleteButton.click();
+        });
     });
 
     test("addMovie function exists", () => {
         render(<App />);
         const addButton = screen.getAllByText(/Add New Movie/i);
         expect(addButton.length > 0);
-        addButton[0].click();
+        act(() => {
+            addButton[0].click();
+        });
         render(<App />);
         const saveButton = screen.getByRole("button", {
             name: /Save Changes/i,
         });
         expect(saveButton);
-        saveButton.click();
+        act(() => {
+            saveButton.click();
+        });
     });
 
     test("cancel function exists", () => {
         render(<App />);
         const addButton = screen.getAllByText(/Edit/i);
         expect(addButton.length > 0);
-        addButton[0].click();
+        act(() => {
+            addButton[0].click();
+        });
         render(<App />);
         const cancelButton = screen.getByRole("button", { name: /Cancel/i });
         expect(cancelButton);
-        cancelButton.click();
+        act(() => {
+            cancelButton.click();
+        });
     });
 });
